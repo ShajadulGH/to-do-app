6a2ba1f5c1981cf7e7232659cf7c8ecf53dfcf68
rahe bhuiyan <shajadulrahe@gmail.com>
2024-02-14 13:37:55 +0600

    feat: add local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
 import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
@@ -10,7 +10,18 @@ const App = () => {
   let content = (
     <p style={{ textAlign: "center" }}>No lists found. Maybe add one?</p>
   );
+  useEffect(() => {
+    // Load tasks from local storage on component mount
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks) {
+      setTodos(storedTasks);
+    }
+  }, []);
 
+  useEffect(() => {
+    // Save tasks to local storage whenever tasks state changes
+    localStorage.setItem("tasks", JSON.stringify(todos));
+  }, [todos]);
   if (todos.length > 0) {
     content = (
       <CourseGoalList
